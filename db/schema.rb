a04eb7eeeb675e17c5697c6e1125bb237fa08b4f
Adam Pash <adam.pash@gmail.com>
2015-09-23 17:44:46 -0400

    updated db

--- db/schema.rb
+++ db/schema.rb
@@ -16,23 +16,6 @@ ActiveRecord::Schema.define(version: 20150923201558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "options", force: :cascade do |t|
-    t.text     "text"
-    t.integer  "poll_id"
-    t.integer  "vote_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "polls", force: :cascade do |t|
-    t.text     "title"
-    t.boolean  "live"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "vote_count"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "name",                   default: "", null: false
@@ -52,13 +35,5 @@ ActiveRecord::Schema.define(version: 20150923201558) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "option_id"
-    t.integer  "poll_id"
-    t.text     "ip_address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
