1931e54b8e081c02d877ef02c0db4b1cdd50568f
Adam Pash <adam.pash@gmail.com>
2015-09-28 18:05:16 -0400

    testing

--- webpack/webpack.rails.config.js
+++ webpack/webpack.rails.config.js
@@ -4,19 +4,19 @@
 
 // NOTE: All style sheets handled by the asset pipeline in rails
 
-const config = require('./webpack.common.config');
-const webpack = require('webpack');
+const config = require('./webpack.common.config')
+const webpack = require('webpack')
 
 config.output = {
   filename: 'client-bundle.js',
   path: '../app/assets/javascripts/components'
-};
+}
 
 // load jQuery from cdn or rails asset pipeline
-// config.externals = {jquery: 'var jQuery'};
+// config.externals = {jquery: 'var jQuery'}
 
 // You can add entry points specific to rails here
-config.entry.push('./scripts/rails_only');
+config.entry.push('./scripts/rails_only')
 
 // See webpack.common.config for adding modules common to both the webpack dev server and rails
 config.plugins = [
@@ -27,7 +27,7 @@ config.plugins = [
     __DEVELOPMENT__: false,
     __DEVTOOLS__: false  // <-------- DISABLE redux-devtools HERE (disable w/false)
   }),
-];
+]
 
 config.module.loaders.push(
   {
@@ -45,14 +45,14 @@ config.module.loaders.push(
   // in the Rails Asset Pipeline. Thus, load this one prior.
   // {test: require.resolve('jquery'), loader: 'expose?jQuery'},
   // {test: require.resolve('jquery'), loader: 'expose?$'}
-);
-module.exports = config;
+)
+module.exports = config
 
 // Next line is Heroku specific. You'll have BUILDPACK_URL defined for your Heroku install.
-// const devBuild = (typeof process.env.BUILDPACK_URL) === 'undefined';
+// const devBuild = (typeof process.env.BUILDPACK_URL) === 'undefined'
 // if (devBuild) {
-//   console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-//   module.exports.devtool = 'eval-source-map';
+//   console.log('Webpack dev build for Rails') // eslint-disable-line no-console
+//   module.exports.devtool = 'eval-source-map'
 // } else {
-console.log('Webpack production build for Rails'); // eslint-disable-line no-console
+console.log('Webpack production build for Rails') // eslint-disable-line no-console
 // }
