55216f46c52cb40a23de8a0283211fa7df055ae2
Adam Pash <adam.pash@gmail.com>
2015-09-28 11:56:45 -0400

    switched out slider test

--- webpack/assets/javascripts/components/Gallery.js
+++ webpack/assets/javascripts/components/Gallery.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import Slider from 'react-slick'
+import SwipeableViews from 'react-swipeable-views'
 
 export default class Gallery extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0
+    }
+  }
+
   renderImages() {
     let { pics } = this.props
     return pics.map( (pic, index) => {
-      console.log(pic)
       return (
         <div key={ index } style={ styles.img_container }>
           <img src={ pic.url } style={ styles.img } />
@@ -13,11 +19,37 @@ export default class Gallery extends React.Component {
       )
     })
   }
+
+  nextImage() {
+    let { index } = this.state
+    let { pics } = this.props
+    this.setState({
+      index: Math.min(index + 1, pics.length - 1)
+    })
+  }
+
+  prevImage() {
+    this.setState({
+      index: Math.max(0, this.state.index - 1)
+    })
+  }
+
   render() {
+    let { index } = this.state
     return (
-      <Slider dots={true}>
-        { this.renderImages() }
-      </Slider>
+      <div style={ styles.container }>
+        <div
+          style={ styles.left }
+          onClick={ this.prevImage.bind(this) }
+        />
+        <SwipeableViews index={ index }>
+          { this.renderImages() }
+        </SwipeableViews>
+        <div
+          style={ styles.right }
+          onClick={ this.nextImage.bind(this) }
+        />
+      </div>
     )
       // <ReactSwipe continuous={ false }>
       // </ReactSwipe>
@@ -25,12 +57,63 @@ export default class Gallery extends React.Component {
 }
 
 const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'stretch',
+  },
   img_container: {
-    textAlign: 'center'
+    textAlign: 'center',
   },
   img: {
     margin: '0 auto',
     display: 'block',
     maxWidth: '100%'
+  },
+  left: {
+    background: 'black',
+    width: 50,
+    opacity: 0.2,
+  },
+  right: {
+    background: 'black',
+    width: 50,
+    opacity: 0.2,
   }
 }
+
+// import React from 'react'
+// import Slider from 'react-slick'
+// 
+// export default class Gallery extends React.Component {
+//   renderImages() {
+//     let { pics } = this.props
+//     return pics.map( (pic, index) => {
+//       console.log(pic)
+//       return (
+//         <div key={ index } style={ styles.img_container }>
+//           <img src={ pic.url } style={ styles.img } />
+//         </div>
+//       )
+//     })
+//   }
+//   render() {
+//     return (
+//       <Slider dots={true}>
+//         { this.renderImages() }
+//       </Slider>
+//     )
+//       // <ReactSwipe continuous={ false }>
+//       // </ReactSwipe>
+//   }
+// }
+// 
+// const styles = {
+//   img_container: {
+//     textAlign: 'center'
+//   },
+//   img: {
+//     margin: '0 auto',
+//     display: 'block',
+//     maxWidth: '100%'
+//   }
+// }
