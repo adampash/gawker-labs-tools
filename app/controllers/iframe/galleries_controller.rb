14e145da6399c26e9461ced218ecf33934bd3872
Adam Pash <adam.pash@gmail.com>
2015-09-28 19:34:07 -0400

    allow iframe

--- app/controllers/iframe/galleries_controller.rb
+++ app/controllers/iframe/galleries_controller.rb
@@ -1,8 +1,15 @@
 class Iframe::GalleriesController < ApplicationController
+  after_action :allow_iframe
   layout 'iframe'
 
   def empty
     render :text => "", :layout => 'iframe'
+  end
+
+
+  private
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
 
