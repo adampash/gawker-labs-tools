c8d5bf6bc55e5516e93af2ce9c3ea0d48b993d45
Adam Pash <adam.pash@gmail.com>
2015-09-28 19:28:10 -0400

    updated to exclude authentication on show

--- app/controllers/api/galleries_controller.rb
+++ app/controllers/api/galleries_controller.rb
@@ -1,5 +1,5 @@
 class Api::GalleriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :show
 
   def index
     @galleries = current_user.latest_galleries
