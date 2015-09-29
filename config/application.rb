1f713a80c1fec9f0efd45f5a2bdb4674395b45e4
Adam Pash <adam.pash@gmail.com>
2015-09-29 12:28:30 -0400

    getting rid of cookies when caching

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,11 @@ module RailsReactWebpackStarter
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # gt rid of cookies when caching
+    config.middleware.insert_before(
+      ExampleMiddleware,
+      "FastlyRails::Rack::RemoveSetCookieHeader"
+    )
   end
 end
