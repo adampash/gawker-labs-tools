f283b80035741626b600eeb6eefaf191cd2edc6a
Adam Pash <adam.pash@gmail.com>
2016-01-15 16:35:01 -0500

    require gawker email to login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,18 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if User.whitelisted? request.env["omniauth.auth"]["info"]["email"]
+      @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      flash[:alert] = "You need to log in with your Gawker Media account"
+      redirect_to root_path
     end
   end
 end
